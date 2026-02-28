ce2a970d048100c07979fc1f8dec347eb22ee987
KallaPavan333 <kallaanand956@gmail.com>
2026-02-28 17:32:50 +0530

    Refactor CseControllerTest to update test method

--- src/test/java/com/example/demo/CseControllerTest.java
+++ src/test/java/com/example/demo/CseControllerTest.java
@@ -1,18 +1,20 @@
 package com.example.demo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CseControllerTest {
-	@Autowired
-	CseController c;
-	@Test
-	void test() {
-		int result = c.addCSE(2, 3);
-		assertEquals(5, result);
-	}
 
+    @Autowired
+    CseController c;
+
+    @Test
+    void testAddCSE() {
+        String result = c.addCSE(2, 3);
+        assertEquals("5 23mh1a05h3", result);
+    }
 }
